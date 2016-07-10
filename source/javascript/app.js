a845a7e709ecf201c29d4fe1c48b0007c239cff0
Pepe Miyashiro <jmiyashiro@authxconsulting.com>
2016-07-10 02:53:29 -0500

    javascript tasks added

---
+++ source/javascript/app.js
@@ -0,0 +1,23 @@
+// // global.jQuery = require('./vendor/jquery-1.11.3');
+// // var ClassTest =  require('./inc/ClassTest');
+// import ClassTest from './inc/ClassTest';
+// // var $ = jQuery;
+//
+const test = require('./inc/test');
+//
+const MAX_HEIGHT = 100;
+console.log(MAX_HEIGHT);
+
+//
+console.log(test(2));
+//
+// // console.log( $ );
+//
+//
+// let ct = new ClassTest();
+//
+// ct.sayHi("Pepe2");
+
+let es6 =  "escma2015";
+
+console.log(es6);
